68730e24bc8768441923abf053e291c2e8fe0f88
victor fernando <victorfernandomagalhaes@gmail.com>
2023-09-23 18:17:45 -0300

    fix

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -12,6 +12,8 @@ use Psr\Log\LoggerInterface;
 /**
  * Class BaseController
  *
+ * 
+ * 
  * BaseController provides a convenient place for loading components
  * and performing functions that are needed by all your controllers.
  * Extend this class in any new controllers:
