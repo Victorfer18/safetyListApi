f3aca6a582d9d72714448b3948ccb5802f813515
victor fernando <victorfernandomagalhaes@gmail.com>
2023-11-15 17:32:21 -0300

    fix

--- app/Controllers/InspectionController.php
+++ app/Controllers/InspectionController.php
@@ -257,6 +257,7 @@ class InspectionController extends BaseController
         $this->db->table('maintenance_file')->insert([
             'system_maintenance_id' => $system_maintenance_id,
             'maintenance_file_path' => $uploadFile,
+            'is_according' => $consistency_status ? 1 : 0
         ]);
 
         return $this->successResponse(INFO_SUCCESS);
