be30098e76f4d9d8c239a62ef8fe309caef04e12
victor fernando <victorfernandomagalhaes@gmail.com>
2023-12-11 18:16:46 -0300

    fix

--- app/Controllers/InspectionController.php
+++ app/Controllers/InspectionController.php
@@ -143,7 +143,7 @@ class InspectionController extends BaseController
             ->join('system_group GRP', 'GRP.system_group_id = TYP.system_group_id')
             ->join('sys_inspection SYSP', 'SYSP.system_type_id = SYS.system_type_id and SYSP.client_id = CLI.client_parent')
             ->whereIn('SYS.client_id', $clientIds)
-            ->where('SYSP.inspection_id', $inspection_id)
+            // ->where('SYSP.inspection_id', $inspection_id)
             ->where('SYS.situation_id', 1)
             ->where('TYP.situation_id', 1)
             ->where('TYP.is_safetyList', 1)
