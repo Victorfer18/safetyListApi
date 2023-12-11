d424f8bbf02af67e566db739d806cd316a54a608
victor fernando <victorfernandomagalhaes@gmail.com>
2023-12-11 18:36:15 -0300

    fix

--- app/Controllers/InspectionController.php
+++ app/Controllers/InspectionController.php
@@ -144,7 +144,6 @@ class InspectionController extends BaseController
             ->whereIn('SYS.client_id', $clientIds)
             ->where('SYS.situation_id', 1)
             ->where('TYP.situation_id', 1)
-            ->where('TYP.is_safetyList', 1)
             ->get();
 
         $inspectables = $query->getResultArray();
